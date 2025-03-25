6ec70aa55f4ef8ff510d005f326e793f4bf09949
Kshitij131 <1ms23cy032@msrit.edu>
2025-03-25 07:17:34 +0530

    Completed task 4

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+function generateGreeting(user) {
+    const { name} = user;
+    const { age } = user;
+    const { city } = user;
+    return `Hello, my name is ${name}, I'm ${age} years old and I live in ${city}.`;
+}
 
+console.log(generateGreeting({ name: "Alice" , age: 30, city: "New York" }));
